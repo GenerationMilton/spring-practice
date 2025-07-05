c2e36135f897ef0a5435a5449afc9737c9cb681a
Milton Munoz <livemilton@gmail.com>
2025-07-05 18:35:03 -0500

    springboot: :sparkles: writing spring Data JPA entitites & respository to interact with DB tables

---
+++ src/main/java/com/practice/accounts/repository/AccountsRepository.java
@@ -0,0 +1,10 @@
+package com.practice.accounts.repository;
+
+import com.practice.accounts.entity.Accounts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountsRepository extends JpaRepository<Accounts, Long> {
+
+}
