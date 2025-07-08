13f7dadca40bc09353e93de1717f54a522062484
Milton Munoz <livemilton@gmail.com>
2025-07-07 21:45:17 -0500

    springboot: :sparkles: perform input data validations inside accounts microservice

--- src/main/java/com/practice/accounts/dto/AccountsDto.java
+++ src/main/java/com/practice/accounts/dto/AccountsDto.java
@@ -1,6 +1,8 @@
 package com.practice.accounts.dto;
 
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,9 +10,13 @@ import lombok.Setter;
 @Data
 public class AccountsDto {
 
+    @NotEmpty(message = "AccountNumber can not be a null or empty")
+    @Pattern(regexp = "(^$|[*0-9]{10})",message = "Mobile number must be 10 digits")
     private Long accountNumber;
 
+    @NotEmpty(message = "AccountType can not be a null or empty")
     private String accountType;
 
+    @NotEmpty(message = "BranchAddress can not be a null or empty")
     private String branchAddress;
 }
