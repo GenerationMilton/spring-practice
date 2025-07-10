aefc62a6e222ebf0921ea8f96f2d00e283bae059
Milton Munoz <livemilton@gmail.com>
2025-07-09 21:55:13 -0500

    springboot: :sparkles: connect databse with docker and yml file

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
 #Start with a base image containing Java runtime
-FROM openjdk:17-jdk-slim
-#Information aroung who maintains the image
-MAINTAINER livemilton.com
-#add the application's jar to the image
-COPY target/accounts-0.0.1-SNAPSHOT.jar java -jar .\target\accounts-0.0.1-SNAPSHOT.jar
+FROM amazoncorretto:17-alpine-jdk
+
+#Information around who maintains the image
+LABEL "website.name"="livemilton.com"
+
+#Add the application's jar to the image
+COPY target/accounts-0.0.1-SNAPSHOT.jar accounts-0.0.1-SNAPSHOT.jar
+
 #execute the application
-ENTRYPOINT ["java", "-jar", "java -jar .\target\accounts-0.0.1-SNAPSHOT.jar"]
+ENTRYPOINT ["java","-jar","accounts-0.0.1-SNAPSHOT.jar"]
 
--- src/main/java/com/practice/accounts/AccountsApplication.java
+++ src/main/java/com/practice/accounts/AccountsApplication.java
@@ -2,10 +2,15 @@ package com.practice.accounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+//@EnableJpaRepositories(basePackages = "com.practice.accounts.repository")
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class AccountsApplication {
 
 	public static void main(String[] args) {
